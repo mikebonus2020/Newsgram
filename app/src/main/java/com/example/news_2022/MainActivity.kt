39e802814fb96bdb117093430bc7899ea3ece9d7
MIKE <mikebonus777@gmail.com>
2022-08-01 16:08:36 -0400

    Revert "commit 1"
    This reverts commit 71bf29e74943a7529e1e94460b564b70b12d6ea4.

--- app/src/main/java/com/example/news_2022/MainActivity.kt
+++ app/src/main/java/com/example/news_2022/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    // commit 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
