96c9657e1324218b146e33e9a1143795eaa5ccdc
MIKE <mikebonus777@gmail.com>
2022-08-01 16:12:38 -0400

    Revert "Revert "Revert "commit 4"""
    This reverts commit e874e91cff3715131eab48fbe37727ccb4640633.

--- app/src/main/java/com/example/news_2022/MainActivity.kt
+++ app/src/main/java/com/example/news_2022/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     // commit 2
     // commit 3
-    // commit 4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
