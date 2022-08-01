4cc2c24a35060fb87a9849ef022ee60a0d1ed17d
MIKE <mikebonus777@gmail.com>
2022-08-01 16:12:54 -0400

    Revert "commit 3"
    This reverts commit 4da1c54405b559b42fa21d2c04bacd12ade8d894.

--- app/src/main/java/com/example/news_2022/MainActivity.kt
+++ app/src/main/java/com/example/news_2022/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     // commit 2
-    // commit 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
